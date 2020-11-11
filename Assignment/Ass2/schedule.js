7efbff6a2224637c85b9f219be5fe2b96c86e631
chowsychoch <cho.cho@ucdconnect.ie>
2020-11-11 09:45:11 +0000

    updated

---
+++ Assignment/Ass2/schedule.js
@@ -0,0 +1,288 @@
+console.log("hello")
+
+function loadJson(RunFunc, arr) {
+    let xmlhttp = new XMLHttpRequest();
+    let url = "scheduling.json";
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let result = JSON.parse(xmlhttp.responseText);
+            console.log('I am running Func')
+            RunFunc(result, arr)
+        }
+    }
+    xmlhttp.open("GET", url, true)
+    xmlhttp.send()
+
+}
+//call
+loadJson(readJsonFile)
+
+
+function readJsonFile(res) {
+
+    //choose the sate first e.g. 2020-4-26
+    let shown = `<select id="date" name="date" onchange="showFunc()">
+    <option value="">Select</option>`;
+    let day = ""
+    //choose the timeslot after clicking a specific date 
+    let timeSlot = ""
+    // read date 
+    for (let i in res) {
+
+        console.log(res[i].date)
+        console.log(res[i].day)
+        shown += `
+        <option value="${res[i].date}"> ${res[i].date} </option> 
+        `
+        day += `<div class="day">${res[i].day}</div>`
+        // access 0 index of slots e.g. {"ba449917":{"slotId"}:"ba449917"}}
+        slots = res[i].slots
+        console.log(Object.values(slots))
+        slot = Object.values(slots)
+        timeSlot += `<select id="${res[i].day}" name="${res[i].day}"><option value="">Selectssss</option>`
+        console.log('chowsy')
+        for (let key of slot) {
+            console.log(key.time)
+            timeSlot += `<option key="${res[i].day}" value="${key.time}">${key.time}</option>`
+        }
+        timeSlot += `</select>`
+    }
+    console.log(shown)
+    console.log(timeSlot)
+    console.log(document.getElementsByClassName("schedule-picker"))
+    document.getElementsByClassName("schedule-picker")[0].innerHTML = shown + `</select>` + day + timeSlot + `
+    
+    <input type="submit" value="Submit the form"/>
+    `
+}
+
+//Click to show the timeslot of that date 
+function showFunc() {
+    let dateChose = document.getElementById("date").value;
+    console.log(dateChose)
+
+    if (dateChose == "2020-04-25") {
+        console.log('hahha')
+        document.getElementById("Saturday").style.display = "block";
+    } else {
+        document.getElementById("Saturday").style.display = "none"
+    }
+
+    if (dateChose == "2020-04-26") {
+        console.log('hahha')
+        document.getElementById("Sunday").style.display = "block";
+    } else {
+        document.getElementById("Sunday").style.display = "none"
+    }
+
+    if (dateChose == "2020-04-27") {
+        console.log('hahha')
+        document.getElementById("Monday").style.display = "block";
+    } else {
+        document.getElementById("Monday").style.display = "none"
+    }
+
+    if (dateChose == "2020-04-28") {
+        console.log('hahha')
+        document.getElementById("Tuesday").style.display = "block";
+    } else {
+        document.getElementById("Tuesday").style.display = "none"
+    }
+
+    if (dateChose == "2020-04-29") {
+        console.log('hahha')
+        document.getElementById("Wednesday").style.display = "block";
+    } else {
+        document.getElementById("Wednesday").style.display = "none"
+    }
+
+    if (dateChose == "2020-04-30") {
+        console.log('hahha')
+        document.getElementById("Thursday").style.display = "block";
+    } else {
+        document.getElementById("Thursday").style.display = "none"
+    }
+
+
+}
+
+
+const formElem = document.querySelector('form');
+formElem.addEventListener('submit', (e) => {
+    // on form submission, prevent default
+    e.preventDefault();
+    // construct a FormData object, which fires the formdata event
+    new FormData(formElem);
+    // e.reset();
+});
+
+formElem.addEventListener('formdata', (e) => {
+    console.log('formdata fired');
+
+    // Get the form data from the event object
+    let data = e.formData;
+    // console.log(data)
+    // console.log(data.keys())
+    let arr = []
+    for (let value of data.values()) {
+
+        if (value != "") {
+            console.log('resultsssss', value);
+            arr.push(value)
+            // bug wait to be fixed
+            if (arr.length > 2) {
+                alert('You might have enter two or more timeslots. Please unselect the timeslot ')
+            }
+        }
+    }
+    console.log(arr)
+    // arr created above pass as para to loadJson i.e. make sure loadJson has accepted para too
+    loadJson(readUserResult, arr)
+});
+
+
+function readUserResult(res, arr) {
+
+    let wrapper = document.getElementById("UserResult")
+    console.log(arr[0])
+    console.log('I am here')
+    let day = ""
+    let slots = ""
+    let result_slot = ""
+    let btn = ""
+    for (let i in res) {
+        console.log(res[i].date)
+        if (arr[0] == res[i].date) {
+            let userDate = res[i].date
+            console.log('hehehehhe', userDate)
+            // day selected 
+            day += `<div class="day">${res[i].day}</div>`
+
+            //get the date and return to specific result 
+            slots = res[i].slots
+            slots = Object.values(slots)
+            console.log(slots)
+
+
+            for (let slot of slots) {
+
+                console.log(slot.time)
+                if (slot.time == arr[1]) {
+                    ////
+                    for (let session of slot.sessions) {
+                        //print out sessions 
+                        // title, time, location, room type 
+                        result_slot += `<div>${session.title}<div>
+                                <div>${session.time}<div>`
+                        // <div>${session.type}<div>
+                        console.log(session.title)
+                    }
+                    wrapper.innerHTML = result_slot
+                    ///if no input for timeslot arr == 1 
+                } else if (arr.length == 1) {
+                    //check input timeslot 
+                    console.log(slot)
+                    console.log('Yes I ammm herererererer')
+                    console.log(slot.sessions)
+                    for (let session of slot.sessions) {
+                        //print out sessions 
+                        // title, time, location, room type 
+                        result_slot += `<div class="filterDiv ${session.type}"> 
+                        <div>${session.title}</div>
+                        <button name="${session.title} ">More info</button>
+                        <div>${session.room}</div>
+                        <div>${session.time}</div>
+                        <div>${session.type}</div>
+                        </div>
+                        `
+                        console.log(session.title)
+                    }
+                    wrapper.innerHTML = result_slot
+
+                }
+
+
+                //set same name allow only one radio btn to be selected
+                btn = ` <input type="radio" id="paper" name="btn" value="paper" onclick="filterSelection('paper')">
+                <label for="paper">Paper</label>
+                <input type="radio" id="other" name="btn" value="workshop" onclick="filterSelection('other')">
+                <label for="workshop">other except paper</label>
+                <input type="radio" id="all" name="btn" value="all" onclick="filterSelection()">
+                <label for="all">all</label>`
+
+            }
+        }
+
+    }
+    wrapper.innerHTML += btn
+}
+
+// function filterSelection() {
+//     let selecteds = document.getElementsByClassName("filterDiv")
+//     console.log(selecteds)
+//     for (let i of selecteds) {
+//         if (i.style.display != 'none') {
+//             i.style.display = "none"
+//             console.log('heheheh im heere')
+//         }
+//         else (i.style.display = "block")
+//     }
+// }
+
+function filterSelection(x) {
+    //show paper only 
+    console.log(x)
+
+    let selecteds = document.getElementsByClassName("filterDiv")
+    console.log(selecteds)
+    if (x == "paper") {
+        console.log('here')
+        for (let i of selecteds) {
+            if (i.className == "filterDiv paper") {
+                //do sth 
+                console.log('haha paper')
+                i.style.display = "block"
+            }
+            else if (i.className !== "filterDiv paper") {
+                i.style.display = "none"
+            }
+            // if (i.style.display !='none'){
+            //     i.style.display = "none"
+            //     console.log('heheheh im heere')
+            // } 
+            // else (i.style.display = "block")
+        }
+    }
+    else if (x == "other") {
+        for (let i of selecteds) {
+            if (i.className == "filterDiv paper") {
+                //do sth 
+                console.log('haha paper')
+                i.style.display = "none"
+            }
+            else if (i.className !== "filterDiv paper") {
+                i.style.display = "block"
+            }
+            // if (i.style.display !='none'){
+            //     i.style.display = "none"
+            //     console.log('heheheh im heere')
+            // } 
+            // else (i.style.display = "block")
+        }
+    }
+    else {
+        for (let i of selecteds) {
+            if (i.style.display == 'none') {
+                i.style.display = "block"
+
+            }
+        }
+
+    }
+
+}
+// give all the div string
+//
+
+
+
---
+++ Assignment/ChoMan_Cho_A1_COMP30680/readme.md
@@ -0,0 +1,17 @@
+This is a personal website for myself. It contains 5 html files including Home, About, Work & Play, Projects, Contact
+
+For the nav bar, unordered list element is used to create a horizontal nav bar. The nav bar is across the five html pages to link to each other.
+
+For index.html, background image is used with the effect of opacity. In order to create a clear contrast of background and fonts. z-index property is used to create font at the top layer. Also, button to link to the About.html to let user know about me. 
+
+For about.html, it consist of 4 parts. Brief intro., Skill Set, Timeline and courses I m taking. The image is scaled with the overflow property. As I have a number of images that is different size and shapes. So I make them a circle by using border radius element and overflow set to hidden. By scaling the size of image. width percentage and margin is used to control the size and position of image. Also, I put images link to external webpages with <a> tag For skills set, I created four bars to indicate different levels and I add an effect of load animation for two second so the percentage of skills is gradually increase when user load the pages. For timeline, this is used mainly with flex model to align the box in a vertical column. Finally, Table is showed to describe the course im taking. 
+
+For Work & Play, a set of images using a card design is show. To align the the card, display for inline-block is used. And image is scaled with overflow property. Also, hover effect is used when user mouses stop at cards. 
+
+For Project.html, a flex model is used to align the content of description and image/video. I embedded video From Youtube. And image to link to external instagram pages.
+
+Finally, contact html, a simple form is used for that with submit button.
+
+I explored additional css and html element by using stackoverflow, reading w3school and reading some blog on medium.
+
+
